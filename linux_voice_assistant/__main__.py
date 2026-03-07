e084c720a45fbaac61ed4f219ce76e31c57c3a77
phieb <code@phieb.at>
2026-03-07 23:21:00 +0100

    fixing broken audio recording

--- linux_voice_assistant/__main__.py
+++ linux_voice_assistant/__main__.py
@@ -448,7 +448,7 @@ def process_audio(state: ServerState, device: Optional[str], block_size: int):
                                 micro_features = MicroWakeWordFeatures()
 
                             micro_inputs.clear()
-                            micro_inputs.extend(micro_features.process_streaming(audio_chunk))
+                            micro_inputs.extend(micro_features.process_streaming(np.frombuffer(audio_chunk, dtype=np.int16)))
 
                             stopped = False
                             for micro_input in micro_inputs:
@@ -486,13 +486,14 @@ def process_audio(state: ServerState, device: Optional[str], block_size: int):
                         state.satellite.handle_audio(audio_chunk)
 
                         assert micro_features is not None
+                        audio_array = np.frombuffer(audio_chunk, dtype=np.int16)
                         micro_inputs.clear()
-                        micro_inputs.extend(micro_features.process_streaming(audio_chunk))
+                        micro_inputs.extend(micro_features.process_streaming(audio_array))
 
                         if has_oww:
                             assert oww_features is not None
                             oww_inputs.clear()
-                            oww_inputs.extend(oww_features.process_streaming(audio_chunk))
+                            oww_inputs.extend(oww_features.process_streaming(audio_array))
 
                         for wake_word in wake_words:
                             activated = False
